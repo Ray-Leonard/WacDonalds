4cbc81656638bfcd8f7d9d04e83cb9fddbff46ef
Ray-Leonard <rayleonard726@gmail.com>
2024-02-19 01:33:39 -0500

    delivery manager complete

--- Assets/_Scripts/CounterScripts/DeliveryCounter.cs
+++ Assets/_Scripts/CounterScripts/DeliveryCounter.cs
@@ -12,6 +12,8 @@ public class DeliveryCounter : BaseCounter
             // delivery counter will only accept plates.
             if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             {
+                DeliveryManager.Instance.DeliverOrder(plateKitchenObject);
+
                 // destroy the plate at player's hand (work the same as a trach counter)
                 player.GetKitchenObject().DestroySelf();
             }
--- Assets/_Scripts/DeliveryManager.cs
+++ Assets/_Scripts/DeliveryManager.cs
@@ -1,10 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
-    [SerializeField] private MenuSO menuSO;
+    public static DeliveryManager Instance { get; private set; }
 
-    private List<OrderRecipeSO> waitingRecipeSOList;
+    [SerializeField] private MenuSO menuSO;             // contains all the recipe SO.
+
+    private List<OrderRecipeSO> waitingOrderSOList;    // for all the recipes that's waiting to be completed.
+
+    private float spawnOrderTimer;
+    private float spawnOrderTimerMax = 4f;
+    private int waitingOrdersMax = 4;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this; 
+
+        waitingOrderSOList= new List<OrderRecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnOrderTimer -= Time.deltaTime;
+        if(spawnOrderTimer <= 0f)
+        {
+            // reset timer
+            spawnOrderTimer = spawnOrderTimerMax;
+
+            if(waitingOrderSOList.Count < waitingOrdersMax)
+            {
+                // get a new recipe from menu and add to waitingOrderList
+                OrderRecipeSO waitingRecipeSO = menuSO.orderRecipeSOList[Random.Range(0, menuSO.orderRecipeSOList.Count)];
+                waitingOrderSOList.Add(waitingRecipeSO);
+                Debug.Log("New Order: " + waitingRecipeSO.name);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Called when a order is delivered to validate if the order is correct.
+    /// Simply cycle through the waitingOrderSOList and see if the kitchenObjectSO on the plate is contained in any of the waiting orders
+    /// </summary>
+    /// <param name="plateKitchenObject">The order will be delivered on a plate</param>
+    public void DeliverOrder(PlateKitchenObject plateKitchenObject)
+    {
+        List<KitchenObjectSO> plateKitchenObjectSOList = plateKitchenObject.GetKitchenObjectSOList();
+
+        for(int i = 0; i < waitingOrderSOList.Count; i++)
+        {
+            OrderRecipeSO waitingOrderRecipeSO = waitingOrderSOList[i];
+
+            // first check if the order and plate has the same number of ingredients. If checked, then proceed to check the ingredients. 
+            if(waitingOrderRecipeSO.kitchenObjectSOList.Count == plateKitchenObjectSOList.Count)
+            {
+                bool isPlateContentMatchesRecipe = true;
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingOrderRecipeSO.kitchenObjectSOList)
+                {
+                    if (!plateKitchenObjectSOList.Contains(recipeKitchenObjectSO))
+                    {
+                        // plate does not contain the order's ingredient, ingredient not found, therefore plate content does not match this recipe.
+                        isPlateContentMatchesRecipe = false;
+                        break;
+                    }
+                }
+
+
+                // now if the previous foreach loop does not set isPlateContentMatchesRecipe to false, 
+                // that means the plate matches with the order
+                if(isPlateContentMatchesRecipe)
+                {
+                    // player delivered the correct order
+                    Debug.Log("Player delivered the correct order");
+
+                    // remove the order from the waiting list.
+                    waitingOrderSOList.RemoveAt(i);
+                    // does not need to continue the search
+                    return;
+                }
+            }
+        }
+
+        // otherwise, if none of the search finds the correct order, it is a wrong delivery.
+        Debug.Log("Player did not deliver a correct recipe");
+    }
 }
